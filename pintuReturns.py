f025259d79ac513a94da3a4ed8de2a0232bcc095
Kshitij <knigam@tracelink.com>
2018-04-12 12:23:12 +0530

    added two new files

--- pintuReturns.py
+++ pintuReturns.py
@@ -3,22 +3,23 @@ import sched, time
 
 max_time = 3600#the amount of time variable
 browser = webdriver.Chrome()
-browser.get('https://www.google.co.in/maps/dir/12.914520,+77.680957/12.926112,+77.686034/@12.9186734,77.6646767,14z/data=!4m10!4m9!1m3!2m2!1d77.680957!2d12.91452!1m3!2m2!1d77.686034!2d12.926112!3e0')
+browser.get('https://www.google.co.in/maps/dir/12.926112,+77.686034/12.914520,+77.680957/@12.9206023,77.6670401,15z/data=!3m1!4b1!4m10!4m9!1m3!2m2!1d77.686034!2d12.926112!1m3!2m2!1d77.680957!2d12.91452!3e0')
 timestr = time.strftime("%Y%m%d-%H%M%S")
-file1 = open("Traffic"+timestr+".csv","w+")
+filename = "Traffic"+timestr+"returnS.csv"
+file1 = open(filename,"w+")
 file1.write("time,Route1-Ambalipura - Sarjapur Rd/Sarjapur Main Rd and NH 44,Route2-Ambalipura - Sarjapur Rd/Sarjapur Main Rd and NH 44\n")   
 s = sched.scheduler(time.time, time.sleep)
 file1.close()
-ambl = "Ambalipura - Sarjapur Rd/Sarjapur Main Rd and NH 44"
-ddkne ="Doddakannelli - Kaadubeesanahalli Rd"
+place1 = "Ambalipura - Sarjapur Rd/Sarjapur Main Rd"
+place2 ="NH 44 and Ambalipura - Sarjapur Rd/Sarjapur Main Rd"
 start_time = time.time()  # remember when we started
 
 while (time.time() - start_time) < max_time:
-    file1 = open("Traffic"+timestr+".csv","a+")
+    file1 = open(filename,"a+")
     print time.asctime( time.localtime(time.time()) )
     x=time.asctime( time.localtime(time.time()) )
-    ddknelixpath='(//span[contains(text(),"'+ddkne+'")]/parent::h1/parent::div/preceding-sibling::div/div/span)[1]'
-    amblpraxpath='(//span[contains(text(),"'+ambl+'")]/parent::h1/parent::div/preceding-sibling::div/div/span)[1]'
+    ddknelixpath='(//span[text()="'+place1+'"]/parent::h1/parent::div/preceding-sibling::div/div/span)[1]'
+    amblpraxpath='(//span[text()="'+place2+'"]/parent::h1/parent::div/preceding-sibling::div/div/span)[1]'
     print(ddknelixpath)
     print(amblpraxpath)
     ddknel=browser.find_element_by_xpath(ddknelixpath)
